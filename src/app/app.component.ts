da171136b2ad1a95a923f0749d92844d0b096898
Jorge Angel Lopez Corona <jorge alopez@tiempodev.local>
2017-10-26 17:57:04 -0500

    add: add tags for audio,video and image

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  codeVideo:string="xSZkMTM5gag";
+  //fullCodeVideo:string="https://www.youtube.com/embed/"+this.codeVideo;
+  fullCodeVideo:string="https://www.youtube.com/embed/xSZkMTM5gag";
   title = 'app works!';
+  wi:number=560;
+  hi:number=315;
 }
